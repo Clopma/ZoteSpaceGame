93a7883513cb0d3d4c7739dce335035ac64bd397
Alejandro Suárez <asu4net@gmail.com>
2022-06-03 10:49:51 +0200

    Added glew, glfw and glm

---
+++ Zote/Window.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<iostream>
+
+#include<glew.h>
+#include<glfw3.h>
+
+namespace Zote
+{
+	class Window
+	{
+		int width = 800;
+		int height = 600;
+
+	public:
+		Window();
+
+	};
+}
+
